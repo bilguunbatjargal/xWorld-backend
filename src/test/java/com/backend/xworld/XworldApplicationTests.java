8c30ccc197d98fdca73603740fdb9ddd70238e60
xpro <xprobeku@gmail.com>
2020-03-20 16:26:35 -0500

    g

---
+++ src/test/java/com/backend/xworld/XworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.xworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
